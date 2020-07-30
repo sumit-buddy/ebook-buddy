23b4978ae885ab93e224a830041b182782e843b0
Sumit <67678753+Sumit-buddy@users.noreply.github.com>
2020-07-30 09:29:51 +0530

    update contact.php

--- contact.php
+++ contact.php
@@ -1,18 +1,17 @@
 <?php
+if(!empty($_POST["submit"])) {
+	$name = $_POST["name"];
+	$email = $_POST["email"];
+	$content = $_POST["message"];
 
-if (isset($_POST['submit'])) {
-  
-  $name=$_POST['submit'];
-  $mailForm=$_POST['email'];
-  $message=$_POST['message'];
-  
-  $mailTo="sumit143deny@gmail.com";
-  $header="form: ".$mailFrom;
-  $txt="You have received an email from ".$name.".\n\n".$message;
-  
-  mail($mailTo,$txt,$headers);
-  headers("Location: index.php?mailsend");
-  
+	$toEmail = "sumit143deny@gmail.com";
+	$mailHeaders = "From: " . $name . "<". $email .">\r\n";
+	if(mail($toEmail, $content, $mailHeaders)) {
+	    $message = "Your contact information is received successfully.";
+	    $type = "success";
+	}
 }
+require_once "contact-view.php";
 ?>
+
 

b457500afa3946fe333ea04e2b924e09db7edffa
Sumit <67678753+Sumit-buddy@users.noreply.github.com>
2020-07-31 08:47:37 +0530

    Delete contact.php

--- contact.php
+++
@@ -1,6 +0,0 @@
-<?php
-if"email" variable is filled out, send email   if (isset($_REQUEST['email']))  {
-  Email information   $admin_email = "sumit143deny@gmail.com";   $email = $_REQUEST['email'];      $comment = $_REQUEST['message'];
-  send email   mail($admin_email, "$subject", $comment, "From:" . $email);
-  Email response   echo "Thank you for contacting us!";   } 
-?>
